a978b241763f494cdaaf7e455747a5611dcaf6f1
Tristan Kuehn <tkuehn@uwo.ca>
2022-10-13 13:52:03 -0400

    Use CBOR for waveforms (for now)

--- adwi.py
+++ adwi.py
@@ -1,34 +1,58 @@
 """Read and assemble aDWI files."""
 
+from __future__ import annotations
 import io
-from itertools import chain, combinations
+from os import PathLike
 import re
 
 from attrs import define
+import cbor2
 import click
 import numpy as np
 import pandas as pd
 
 
-def read_waveforms(path):
+def read_waveforms(path: PathLike) -> tuple[pd.DataFrame]:
     """Read waveform file into a dataframe."""
-    return pd.read_csv(
-        path, sep=" ", skiprows=1, header=None, comment="#", names=["x", "y", "z"]
+    with open(path, "r", encoding="utf-8") as waveform_file:
+        waveform_lines: list[str] = waveform_file.readlines()
+    try:
+        waveform_2_start: int | None = waveform_lines.index("XYDAT2= (T X Y Z)")
+    except ValueError:
+        waveform_2_start = None
+    if waveform_2_start is None:
+        waveform: pd.DataFrame = pd.read_csv(
+            path, sep=" ", skiprows=1, header=None, comment="#", names=["x", "y", "z"]
+        )
+        return (waveform, waveform)
+    waveform_1: pd.DataFrame = pd.read_csv(
+        io.StringIO("\n".join(waveform_lines[:waveform_2_start])),
+        skiprows=1,
+        header=None,
+        comment="#",
+        names=["x", "y", "z"],
     )
+    waveform_2: pd.DataFrame = pd.read_csv(
+        io.StringIO("\n".join(waveform_lines[waveform_2_start + 1 :])),
+        header=None,
+        comment="#",
+        names=["x", "y", "z"],
+    )
+    return (waveform_1, waveform_2)
 
 
-def read_vols(path):
+def read_vols(path: PathLike) -> pd.DataFrame:
     """Read volume file into a dataframe."""
     return pd.read_csv(path, sep=":", comment="#", names=["vol_idx", "waveform_idx"])
 
 
-def read_dirs(path):
+def read_dirs(path: PathLike) -> pd.DataFrame:
     """Parse and read direction file into a dataframe."""
     dir_pattern = (
         r"Vector\[\d+\] = \( (-?\d+(?:\.\d+)?, -?\d+(?:\.\d+)?, -?\d+(?:\.\d+)?) \)"
     )
     with open(path, "r", encoding="utf-8") as dir_file:
-        lines = list(dir_file.readlines())
+        lines: list[str] = list(dir_file.readlines())
     return pd.read_csv(
         io.StringIO(
             "\n".join([re.match(dir_pattern, line).group(1) for line in lines[3:]])
@@ -38,7 +62,7 @@ def read_dirs(path):
     )
 
 
-def rot_matrix(u_vec, theta):
+def rot_matrix(u_vec, theta: float):
     """Calculate a rotation matrix from and axis of rotation and angle.
 
     Parameters
@@ -123,52 +147,53 @@ def build_rot_table(orig, dirs):
 
 
 @define
-class OscillatingGradientPair:
-    time_offset: int = 0
-    polarity: 1 | -1
-    time_delta: int
-    time_rise: list[list[int]]
-    time_fall: list[list[int]]
-    time_plateau: list[list[int]]
-    amplitude: list[list[float]]
+class BinaryWave:
+    """Binary representation of a gradient waveform."""
+    grad1: pd.DataFrame
+    grad2: pd.DataFrame
 
-    def __attrs_post_init__(self):
-        if any(
-            len(first) != len(second)
-            for first, second in combinations(
-                [self.time_rise, self.time_fall, self.time_plateau, self.amplitude], 2
-            )
-        ):
-            raise ValueError(
-                "time_rise, time_fall, time_plateau, and amplitude all need the "
-                "same length"
-            )
-        for sublist in chain(
-            self.time_rise, self.time_fall, self.time_plateau, self.amplitude
-        ):
-            if len(sublist) != 3:
-                raise ValueError("All sublists must be length 3")
+    def as_dict(self) -> dict[str, list[float]]:
+        """Represent the waveform as a dictionary."""
+        return {
+            "xgrad1": list(self.grad1.x),
+            "ygrad1": list(self.grad1.y),
+            "zgrad1": list(self.grad1.z),
+            "xgrad2": list(self.grad2.x),
+            "ygrad2": list(self.grad2.y),
+            "zgrad2": list(self.grad2.z),
+        }
 
-    def gen_waveform(self, sampling_period, length):
-        arr_out = pd.DataFrame(
-            np.zeros([length // sampling_period, 3]), columns=["x", "y", "z"]
-        )
-        return arr_out
+    def write_cbor(self, out_path: PathLike):
+        """Write the waveform dict to a CBOR file."""
+        with open(out_path, "wb") as out_file:
+            cbor2.dump(self.as_dict(), out_file)
 
 
 class SpecificationError(Exception):
     """Error raised when the diffusion scheme is misspecified."""
 
 
-@click.command()
-@click.argument("dirs_path")
-@click.argument("vols_path")
-@click.argument("out_path")
-def main(dirs_path, vols_path, out_path):
+@click.group()
+def cli():
+    """Click group for subcommands."""
+
+
+@cli.command()
+@click.argument("waveform_path", type=click.Path(exists=True))
+@click.argument("out_path", type=click.Path())
+def gen_waveform_cbor(waveform_path: PathLike, out_path: PathLike):
+    """Read a waveform from a file and write a CBOR file describing it."""
+    BinaryWave(*read_waveforms(waveform_path)).write_cbor(out_path)
+
+
+@cli.command()
+@click.argument("dirs_path", type=click.Path(exists=True))
+@click.argument("vols_path", type=click.Path(exists=True))
+@click.argument("out_path", type=click.Path())
+def gen_dir_table(dirs_path: PathLike, vols_path: PathLike, out_path: PathLike):
     """Read directions from a file and write a file with rotations."""
     dirs = read_dirs(dirs_path)
     vols = read_vols(vols_path)
-    print(vols)
     if vols.shape[0] % dirs.shape[0] != 0:
         raise SpecificationError("Volume and direction table lengths don't match up.")
     dir_repetitions = vols.shape[0] // dirs.shape[0]
@@ -183,4 +208,4 @@ def main(dirs_path, vols_path, out_path):
 
 
 if __name__ == "__main__":
-    main()
+    cli()
